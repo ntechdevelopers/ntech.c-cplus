3c9a542284d30bf2cf6b3e402d83202b49d777a7
vuthanhnam94 <vuthanhnam94@gmail.com>
2021-11-19 19:53:33 +0700

    [Dev] greatest_common_divisor

---
+++ Basic/greatest_common_divisor/greatest_common_divisor.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+#define vao "greatest_common_divisor.inp"
+#define ra "greatest_common_divisor.out"
+
+int main()
+{
+	  //khai bao
+	  FILE *f1,*f2;
+	  int a,b;
+	
+	  // nhap du lieu
+	  f1=fopen (vao,"r");
+	 
+	  fscanf (f1,"%d%d",&a,&b);
+	
+	  fclose (f1);
+	  //xu ly va xuat du lieu
+	  f2=fopen (ra,"w");
+	  if (a>=0&&a<=12&&b>0)
+	  {
+	       switch (a)
+	      {
+	         case 1 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 2 :
+	              if (b%4==0)
+	              	fprintf (f2,"thang %d nam %d co 29 ngay",a,b);
+	              else 
+				  	fprintf (f2,"thang %d nam %d co 28 ngay",a,b);
+	              break;
+	         case 3 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 4 : fprintf (f2,"thang %d nam %d co 30 ngay",a,b);break;
+	         case 5 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 6 : fprintf (f2,"thang %d nam %dco 30 ngay",a,b);break;
+	         case 7 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 8 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 9 : fprintf (f2,"thang %d nam %d co 30 ngay",a,b);break;
+	         case 10 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	         case 11 : fprintf (f2,"thang %d nam %d co 30 ngay",a,b);break;
+	         case 12 : fprintf (f2,"thang %d nam %d co 31 ngay",a,b);break;
+	     }
+	  }  
+	  else fprintf (f2,"ban nhap khong dung");
+	  fclose (f2);
+
+	  return 0;
+}
